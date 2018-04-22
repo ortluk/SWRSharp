3f5ce651e60830f832e36843d93ef7790ea4d1d5
Ortluk <ortluk@hotmail.com>
2018-04-21 22:20:49 -0600

    initial commit

---
+++ SWRSharp/Client.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SWRSharp
+{
+    public class Client
+    {
+        private NetworkStream clientStream;
+        private TcpClient tcpClient;
+        private StringBuilder inBuffer;
+        private String Command;
+        private bool commandPending;
+
+        public Client(TcpClient inClient)
+        {
+            tcpClient = inClient;
+            clientStream = tcpClient.GetStream();
+            inBuffer = new StringBuilder();
+            commandPending = false;
+
+        }
+
+        public void Send(String msg)
+        {
+            if (clientStream.CanWrite)
+            {
+                byte[] buffer = Encoding.UTF8.GetBytes(msg);
+                clientStream.Write(buffer,0,buffer.Length);
+            }
+        }
+
+        public String GetCommand()
+        {
+            String ReturnValue = String.Copy(Command);
+            Command = "";
+            commandPending = false;
+            return ReturnValue;
+        }
+
+        public bool isCommandPending()
+        {
+            return commandPending;
+        }
+        
+        public bool AttemptRead()
+        {
+            
+            if (clientStream.CanRead && tcpClient.Client.Poll(1,SelectMode.SelectRead))
+            {
+                byte[] ReadBuffer = new byte[1024];
+                int bytesread = 0;
+
+                do
+                {
+                    try
+                    {
+                        if ((bytesread = clientStream.Read(ReadBuffer, 0, ReadBuffer.Length)) != 0)
+                        {
+                            inBuffer.AppendFormat("{0}", Encoding.ASCII.GetString(ReadBuffer, 0, bytesread));
+                            if (ReadBuffer[bytesread- 1] == '\r' || ReadBuffer[bytesread - 1] == '\n')
+                            {
+                                if (!commandPending)
+                                {
+                                    Command = inBuffer.ToString();
+                                    inBuffer.Clear();
+                                    commandPending = true;
+                                    return true;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    catch (IOException e)
+                    {
+                        return false;
+                    }
+                } while (clientStream.DataAvailable);
+            }
+            return true;
+        }
+        public void Close()
+        {
+            tcpClient.Close();
+            clientStream.Close();
+        }
+        
+    }
+}
---
+++ SWRSharp/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+
+namespace SWRSharp
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<Client> ClientList = new List<Client>();
+            
+            Console.WriteLine("Listening...");
+            TcpListener server = new TcpListener(IPAddress.Any, 4000);
+            server.Start();
+            while (true)
+            {
+                if (server.Pending())
+                {
+                    Client connection = new Client( server.AcceptTcpClient());
+                    connection.Send("Welcome Message.\r\n");
+                    Console.WriteLine("connection accepted.");
+                    ClientList.Add(connection);
+                }
+
+                Client client;
+                for (int i = ClientList.Count - 1; i > -1; i--)
+                {
+                    client = ClientList[i];
+                    if (!client.AttemptRead())
+                    {
+                        Console.WriteLine("Client Disconnected...");
+                        ClientList.RemoveAt(i);
+                        client.Close();
+                    }
+
+                    if (client.isCommandPending())
+                    {
+                        String Command = client.GetCommand();
+
+                        switch (Command)
+                        {
+                            case "Quit\r\n":
+                                Console.WriteLine("Client Disconnected...");
+                                ClientList.RemoveAt(i);
+                                client.Close();
+                                break;
+                            case "Shutdown\r\n":
+                                server.Stop();
+                                for (int x = ClientList.Count - 1; x > -1; x--)
+                                {
+                                    Console.WriteLine("Client Disconnected...");
+                                    ClientList.RemoveAt(x);
+                                    client.Close();
+                                }
+                                Console.WriteLine("Server Stopped");
+                                return;
+                            default:
+                                client.Send(Command);
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
